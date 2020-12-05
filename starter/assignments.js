2feda4bffdcecf69d8734c940f1af7dd4a04537b
Ilja <iljakristafenko@gmail.com>
2020-12-05 20:24:48 +0200

    JS learn Jons

---
+++ starter/assignments.js
@@ -0,0 +1,137 @@
+// const country = "Lavtija";
+// const continent = "Europa";
+// let isIlsland = false;
+// let language;
+// let population = 16; 
+
+// language = "latvian";
+// isIlsland = true;
+
+
+// console.log(typeof isIlsland);
+// console.log(typeof language);
+// console.log(typeof country);
+// console.log(continent);
+// console.log(typeof population);
+// console.log(typeof isIlsland);
+// //!-------------------------------------------------------- 1
+
+// population = population / 2;
+// population++;
+// console.log(population);
+
+// let populationOnFinland = 6;
+// let normPopulation = 33;
+// console.log(population > populationOnFinland); // 6
+// console.log(population < normPopulation); // 33
+
+// let description = country +" is in "+ continent + " , and its " + population + " million people speak " + language;
+// console.log(description);
+
+// let descriptionNewLiteralSyntax = `${country} is in ${continent}, end it's ${population} million people speak ${language}`;
+// console.log(descriptionNewLiteralSyntax);
+
+//! ------------------------------------------------------  2
+// let massMarks = 78;
+// let heighMarks = 1.69;
+// let massJohn = 92;
+// let heighJohn = 1.95;
+
+// let massMarks = 50;
+// let heighMarks = 1.69;
+// let massJohn = 92;
+// let heighJohn = 1.95;
+
+// let bmiMarks = massMarks / heighMarks ** 2;
+// // let BMI = massMarks / (heighMarks * heighMarks); // other formula
+// let bmiJohn = massJohn / heighJohn ** 2;
+// let markHigherBMI = bmiMarks > bmiJohn;
+
+
+// console.log(bmiMarks, bmiJohn, markHigherBMI);
+//! -----------------------------------------------------------  3
+
+// const firstName = "Ilja";
+// const job = "javaScript programer";
+// const birthYear = 1988;
+// const curentYear = 2020;
+
+// const ilja = `I'm ${firstName}, a ${curentYear - birthYear}, years old ${job} !`;
+
+// console.log(ilja);
+
+// console.log(` dsd
+// sds
+// sdsd          //?   new line in console.  bactiks ````
+// sd`);
+
+//!-------------------------------------------------------------  4 
+
+
+// const age = 14;
+// // const isOldEnough = age >= 18;
+
+// if (age >= 18) {
+//     console.log("Sarah can start driving license 🚗 ");
+// }else {
+//     const earsLeft = 18 - age;
+//     console.log(`You left ${earsLeft} ears`);
+// }
+
+// const birthYear = 2288;
+// let century;
+
+// if (birthYear <= 2000) {
+//     century = 20;             //? variables outside code block. 
+// }else {
+//     century = 21;
+// }
+// console.log(century);
+//! ------------------------------------------------------------- 5 
+
+
+// const country = "Lavtija";
+// const continent = "Europa";
+// let isIlsland = false;
+// let language;
+// let population = 16; 
+
+// language = "latvian";
+// isIlsland = true;
+
+// if (population > 33) {
+//     console.log(`${country} population is above average!`);
+// }else {
+//     console.log(`${country} population is ${33 - population} million below is the avarege!`);
+// }
+
+
+// Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+// 1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+// HINT: Use an if/else statement 😉
+
+// GOOD LUCK 😀
+
+// let massMarks = 78;
+// let heighMarks = 1.69;
+
+// let massJohn = 92;
+// let heighJohn = 1.95;
+
+
+// let bmiMarks = massMarks / heighMarks ** 2;
+// let bmiJohn = massJohn / heighJohn ** 2;
+// // let markHigherBMI = bmiMarks > bmiJohn;
+
+
+// if (bmiMarks >= bmiJohn) {
+//     console.log(`Marks is ${bmiMarks} more big than John ${bmiJohn}!`);
+// } else {
+//     console.log(`John is more big than Marks!`);
+// }
+
+// console.log(bmiMarks, bmiJohn);
+
